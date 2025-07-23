b4fc946a97d8b67c0219285eb59a4349ff935d31
Janos <jtoldy@web.de>
2025-07-23 13:35:35 +0200

    safion v3 working

--- app_pages/safion.py
+++ app_pages/safion.py
@@ -5,84 +5,116 @@ import plotly.graph_objects as go
 def safion_app():
     st.title("Safion Ergebnisse")
     con1 = st.container(border=True)
-    dic = get_data(con1)
-    if len(dic) != 0:
+    uploaded_file = con1.file_uploader("Datei Auswählen", type=["csv"], accept_multiple_files=True)
+    dateinamen = [file.name for file in uploaded_file]
+    if len(uploaded_file) != 0:
         st.subheader("Plot")
-
-        sel = st.selectbox("Daten wählen",dic.keys(),index=None)
+        sel = st.selectbox("Daten wählen",dateinamen,index=None)
         if sel is not None:
-            data = dic[sel]["Daten"]
-            metadata = dic[sel]["Metadaten"]
-            fig = go.Figure()
-            fig.add_trace(go.Scatter(x=data["ReZ"], y=data["ImZ"], name="Niqhist-Kurve",line_shape='spline'))
+            up_file = next((file for file in uploaded_file if file.name == sel), None)
+            dic = get_data(up_file, con1)
+            on = st.toggle("Delete wrong Data",value=True)
 
-            st.plotly_chart(fig)
-            st.write(metadata)
-            with st.expander("Reshaped-Daten anzeigen", expanded=False):
-                st.dataframe(data)
-
-            st.subheader("Frequenzen")
-            fig1 = go.Figure()
-            fig1.add_trace(go.Scatter(y=data["Freq"], name="Frequenezen", line_shape='spline'))
-            st.plotly_chart(fig1)
         else:
-            fig = go.Figure()
-            fig1 = go.Figure()
-            first_key = next(iter(dic.keys()))
-            first_df = dic[first_key]["Metadaten"]
-            meta_col = first_df.columns
-            meta = pd.DataFrame(columns= meta_col)
-            for file in dic:
-                data = dic[file]["Daten"]
-                metadata = dic[file]["Metadaten"]
-                metadata["name"] = file
-                pd.concat([meta, metadata], ignore_index=True)
-                fig.add_trace(go.Scatter(x=data["ReZ"], y=data["ImZ"], name=file, line_shape='spline'))
-                fig1.add_trace(go.Scatter(y=data["Freq"], name=file, line_shape='spline'))
+            dic = get_all_data(uploaded_file,con1)
+            on = False
+        fig = go.Figure()
+        fig1 = go.Figure()
+        first_key = next(iter(dic.keys()))
+        first_df = dic[first_key]["Metadaten"]
+        meta_col = first_df.columns
+        meta = pd.DataFrame(columns= meta_col)
+        for file in dic:
+            data = dic[file]["Daten"]
+            metadata = dic[file]["Metadaten"]
+            metadata["name"] = file
+
+            if metadata["Time [s]"][0] > 20 and on:
+                continue
+            meta = pd.concat([meta, metadata], ignore_index=True)
+            fig.add_trace(go.Scatter(
+                x=data["ReZ"],
+                y=data["ImZ"],
+                name=file,
+                line_shape='spline',
+                customdata=data[["PhaseZ", "Freq"]],
+                hovertemplate="ReZ: %{x}<br>ImZ: %{y}<br>PhaseZ: %{customdata[0]}<br>Freq: %{customdata[1]} Hz"
+                              "<extra></extra>"
+            ))
+            fig1.add_trace(go.Scatter(y=data["Freq"], name=file, line_shape='spline'))
+        if sel is None:
             fig.update_layout(legend=dict(
                 yanchor="bottom",
                 y=1,
                 xanchor="left",
                 x=0.01
             ))
-            fig1.update_layout(legend=dict(
-                yanchor="bottom",
-                y=0.99,
-                xanchor="left",
-                x=0.01
-            ))
-            st.plotly_chart(fig)
-            st.write(meta)
-            st.subheader("Frequenzen")
-            st.plotly_chart(fig1)
+        fig1.update_layout(legend=dict(
+            yanchor="bottom",
+            y=0.99,
+            xanchor="left",
+            x=0.01
+        ))
+        st.plotly_chart(fig)
+        st.write(meta)
+        st.subheader("Frequenzen")
+        st.plotly_chart(fig1)
 
-def get_data(con):
-    uploaded_file = con.file_uploader("Datei Auswählen", type=["csv"], accept_multiple_files=True)
-    if uploaded_file is not None:
-        dic = {}
-        for file in uploaded_file:
-            df = pd.read_csv(file)
+def get_all_data(uploaded_file, con):
+    dic = {}
+    for file in uploaded_file:
+        df = pd.read_csv(file)
 
-            mean_row = df.mean()
+        first_row = df.head(1)
 
-            # Metadaten und Daten trennen
-            meta_columns = df.columns[:13]  # Die ersten 13 Spalten sind Metadaten
-            data_columns = df.columns[13:]  # Die restlichen Spalten sind Daten
+        # Metadaten und Daten trennen
+        meta_columns = df.columns[:13]  # Die ersten 13 Spalten sind Metadaten
+        data_columns = df.columns[13:]  # Die restlichen Spalten sind Daten
 
-            data = []
-            meta_data = [mean_row[meta_columns].values]
-            meta_data = pd.DataFrame(meta_data, columns=meta_columns)
-            for i in range(0, len(data_columns), 4):
-                data_group = mean_row[data_columns[i:i + 4]].values
-                data.append(list(data_group))
+        data = pd.DataFrame()
+        meta_data = first_row[meta_columns].values
+        meta_data = pd.DataFrame(meta_data, columns=meta_columns)
+        for i in range(0, len(data_columns), 4):
+            data_group = first_row[data_columns[i:i + 4]].values
+            data_df = pd.DataFrame(list(data_group))
+            data = pd.concat([data, data_df], ignore_index=True)
 
-            # Erstellen eines neuen DataFrames
-            new_columns = ["ReZ", "ImZ", "PhaseZ", "Freq"]
-            reshaped_df = pd.DataFrame(data, columns=new_columns)
-            reshaped_df["ImZ"] = -reshaped_df["ImZ"]
+        # Erstellen eines neuen DataFrames
+        new_columns = ["ReZ", "ImZ", "PhaseZ", "Freq"]
+        data.columns = new_columns
+        data["ImZ"] = -data["ImZ"]
+        dic[file.name] = {
+            "Metadaten": meta_data,
+            "Daten": data
+        }
+    return dic
 
-            dic[file.name] = {
-                "Metadaten": meta_data,
-                "Daten": reshaped_df
-            }
-        return dic
+def get_data(uploaded_file, con):
+    dic = {}
+
+    df = pd.read_csv(uploaded_file)
+
+    # Metadaten und Daten trennen
+    meta_columns = df.columns[:13]  # Die ersten 13 Spalten sind Metadaten
+    data_columns = df.columns[13:]  # Die restlichen Spalten sind Daten
+
+    new_columns = ["ReZ", "ImZ", "PhaseZ", "Freq"]
+
+    for idx, row in df.iterrows():
+        data = pd.DataFrame()
+
+        meta_data = pd.DataFrame([row[meta_columns].values], columns=meta_columns)
+        for i in range(0, len(data_columns), 4):
+            data_group = row[data_columns[i:i + 4]].values
+            data_df = pd.DataFrame(list(data_group))
+            data = pd.concat([data, data_df], axis=1 ,ignore_index=True)
+        # Erstellen eines neuen DataFrames
+        data = data.T
+        data.columns = new_columns
+        data["ImZ"] = -data["ImZ"]
+
+        dic[meta_data["Time [s]"].values[0]] = {
+            "Metadaten": meta_data,
+            "Daten": data
+        }
+    return dic
