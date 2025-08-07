c572587dec0478adda1dad315f3a80e46a683b77
Janos <jtoldy@web.de>
2025-08-07 18:01:10 +0200

    bode diagramme

--- app_pages/impedanz.py
+++ app_pages/impedanz.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from Classes.datenbank import Database
 import pandas as pd
+import plotly.graph_objects as go
 
 def impedanz_app():
     st.title("Impedanz")
@@ -15,3 +16,16 @@ def impedanz_app():
             data.append(row)
     df = pd.DataFrame(data)
     st.write(df)
+
+    imp_data = []
+    for plot in df.itertuples(index=False):
+        data = DB.get_impedanz(plot.name,plot.c_rate)
+        if data[0].typ == "Basitec":
+            min_time = min(row.time for row in data)
+            filtered_rows = [row for row in data if row.time == min_time]
+        else:
+            filtered_rows = data
+        imp_data.append(filtered_rows)
+    st.write(imp_data)
+
+
