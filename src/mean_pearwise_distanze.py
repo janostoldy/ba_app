547ba5846b584e9bd1ce765265cb0853537e8a0a
Janos <jtoldy@web.de>
2025-09-17 10:39:24 +0200

    mpd

---
+++ src/mean_pearwise_distanze.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def mean_pairwise_abs_diff(x):
+    x = np.asarray(x)
+    x = x[~np.isnan(x)]         # NaNs entfernen falls nötig
+    n = x.size
+    if n < 2:
+        return 0.0
+    x_sorted = np.sort(x)       # O(n log n)
+    k = np.arange(1, n+1)       # 1..n
+    S = (x_sorted * (2*k - n - 1)).sum()
+    mean = 2.0 * S / (n * (n - 1))
+    return mean
