f83a743796d0d5e9766f16f2bee63c358d1616f8
Janos <jtoldy@web.de>
2025-10-02 11:45:49 +0200

    table app

--- app_pages/lup.py
+++ app_pages/lup.py
@@ -20,8 +20,23 @@ def table_app():
     st.title("Creat Look-Up-Table")
     DB = Database("lup")
     df = DB.get_lup()
-    data_df = df[df["freqhz"].between(195, 205)]
-    data_df = data_df[data_df["calc_ima"]==0]
+    df['calc_soc'] = df['soc']/2500 + 0.1
+    plots = ['phasezdeg', 'calc_rezohm', 'calc_imzohm']
+
+    data_df = df[df["calc_ima"]==1250]
+    data_df = data_df[data_df["freqhz"].between(195, 205)]
+    data_df["temperaturec_cat"] = data_df["temperaturec"].astype(str)
+    data_df = data_df.sort_values("soc")
+    for plot in plots:
+        st.subheader(plot + ' 200 Hz')
+        fig = px.scatter(data_df,
+                        x='calc_soc',
+                        y=plot,
+                        color='temperaturec_cat',
+                        hover_data=["datei"],
+                        symbol="temperaturec_cat",
+                         )
+        st.plotly_chart(fig)
 
     fig = px.scatter_3d(data_df,
                         x='soc',
