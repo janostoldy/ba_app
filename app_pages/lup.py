75755fc7dafc89ef958c9711328ebf46626aeb2d
Janos <jtoldy@web.de>
2025-10-02 11:45:42 +0200

    lup app

--- app_pages/lup.py
+++ app_pages/lup.py
@@ -4,6 +4,19 @@ import pandas as pd
 from classes.datenbank import Database
 
 def lup_app():
+    with st.sidebar:
+        side = st.radio(
+            "Wähle eine Option",
+            ("Table", "Formierung", "Fit")
+        )
+    if side == "Table":
+        table_app()
+    elif side == "Formierung":
+        form_app()
+    else:
+        fit_app()
+
+def table_app():
     st.title("Creat Look-Up-Table")
     DB = Database("lup")
     df = DB.get_lup()
